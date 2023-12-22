895ae95a0f8b802ed63724fb32e675a1fdd2fbbe
Poletansky Viktor <poletansky@gmail.com>
2023-12-23 01:57:21 +0300

    [master] u

--- circlegrid.go
+++ circlegrid.go
@@ -10,52 +10,6 @@ type circleGrid struct {
 	circleNumMin, circleNumMax int
 }
 
-func (cg *circleGrid) draw(dc *Context, lineWidth float64, c Canvas, v V2, r float64) {
-	rnd := rand.Intn(4)
-	col := c.ColorSchema[rand.Intn(len(c.ColorSchema))]
-	dc.Stack(func(dc *Context) {
-		dc.Translate(v)
-		dc.Rotate(float64(rand.Intn(10)))
-		dc.SetColor(col)
-		dc.SetLineWidth(lineWidth)
-
-		switch rnd {
-		case 0:
-			dc.DrawCircleV2(0, r)
-			dc.Stroke()
-		case 1:
-			n := RandomRangeInt(1, 4) * 2
-			dc.DrawCircleV2(0, r)
-			dc.Stroke()
-			for i := 0; i < n; i++ {
-				dc.Rotate(math.Pi * 2 / float64(n))
-				dc.DrawCircleV2(complex(r, 0), r*0.1)
-				dc.Fill()
-			}
-		case 2:
-			n := RandomRangeInt(8, 20)
-			theta := math.Pi * 0.5 * float64(RandomRangeInt(1, 5))
-			for i := 0; i < n; i++ {
-				d := float64(i) / float64(n)
-				if d > r*0.1 {
-					d = r * 0.1
-				}
-				dc.Rotate(theta / float64(n))
-				dc.DrawCircleV2(complex(r/2, 0), d*2)
-				dc.Fill()
-			}
-		case 3:
-			n := RandomRangeInt(5, 20)
-			for i := 0; i < n; i++ {
-				dc.Rotate(math.Pi * 2 / float64(n))
-				dc.DrawLine(complex(r/2, 0), complex(r*2/3-r*0.05, 0))
-				dc.Stroke()
-			}
-
-		}
-	})
-}
-
 func (cg *circleGrid) grid(dc *Context, c Canvas) {
 	var segment int = 100
 	w := float64(c.Width) / float64(segment)
@@ -95,7 +49,51 @@ func CircleGrid(c Canvas, lineWidth float64, circleNumMin, circleNumMax int) {
 			dc.SetColor(c.ColorSchema[rand.Intn(len(c.ColorSchema))])
 			dc.DrawCircleV2(v, w/2*RandomFloat64(0.1, 0.5))
 			dc.Fill()
-			cg.draw(dc, lineWidth, c, v, w/2*RandomFloat64(0.6, 0.95))
+			func(dc *Context, lineWidth float64, c Canvas, v V2, r float64) {
+				rnd := rand.Intn(4)
+				col := c.ColorSchema[rand.Intn(len(c.ColorSchema))]
+				dc.Stack(func(dc *Context) {
+					dc.Translate(v)
+					dc.Rotate(float64(rand.Intn(10)))
+					dc.SetColor(col)
+					dc.SetLineWidth(lineWidth)
+
+					switch rnd {
+					case 0:
+						dc.DrawCircleV2(0, r)
+						dc.Stroke()
+					case 1:
+						n := RandomRangeInt(1, 4) * 2
+						dc.DrawCircleV2(0, r)
+						dc.Stroke()
+						for i := 0; i < n; i++ {
+							dc.Rotate(math.Pi * 2 / float64(n))
+							dc.DrawCircleV2(complex(r, 0), r*0.1)
+							dc.Fill()
+						}
+					case 2:
+						n := RandomRangeInt(8, 20)
+						theta := math.Pi * 0.5 * float64(RandomRangeInt(1, 5))
+						for i := 0; i < n; i++ {
+							d := float64(i) / float64(n)
+							if d > r*0.1 {
+								d = r * 0.1
+							}
+							dc.Rotate(theta / float64(n))
+							dc.DrawCircleV2(complex(r/2, 0), d*2)
+							dc.Fill()
+						}
+					case 3:
+						n := RandomRangeInt(5, 20)
+						for i := 0; i < n; i++ {
+							dc.Rotate(math.Pi * 2 / float64(n))
+							dc.DrawLine(complex(r/2, 0), complex(r*2/3-r*0.05, 0))
+							dc.Stroke()
+						}
+
+					}
+				})
+			}(dc, lineWidth, c, v, w/2*RandomFloat64(0.6, 0.95))
 		}
 	}
 }
