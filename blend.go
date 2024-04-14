c95562bfbb0ef2d929b543d318396f55123734d4
Poletansky Viktor <poletansky@gmail.com>
2024-04-14 12:05:59 +0300

    [master] u

--- blend.go
+++ blend.go
@@ -1,6 +1,7 @@
 package gena
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 )
@@ -11,19 +12,25 @@ const (
 	Add BlendMode = iota
 )
 
-func Blend(src, dest image.Image, mode BlendMode) image.Image {
+func BlendAdd(src, dest image.Image) image.Image {
 	img := image.NewRGBA(src.Bounds())
-	switch mode {
-	case Add:
-		for i := range Range(src.Bounds().Max.X) {
-			for j := range Range(src.Bounds().Max.Y) {
-				if r, g, b, a := src.At(i, j).RGBA(); (color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}) == Black {
-					img.Set(i, j, dest.At(i, j))
-				} else {
-					img.Set(i, j, Mix(src.At(i, j), dest.At(i, j)))
-				}
+	for i := range Range(src.Bounds().Max.X) {
+		for j := range Range(src.Bounds().Max.Y) {
+			if r, g, b, a := src.At(i, j).RGBA(); (color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}) == Black {
+				img.Set(i, j, dest.At(i, j))
+			} else {
+				img.Set(i, j, Mix(src.At(i, j), dest.At(i, j)))
 			}
 		}
 	}
 	return img
+}
+
+func Blend(src, dest image.Image, mode BlendMode) image.Image {
+	switch mode {
+	case Add:
+		return BlendAdd(src, dest)
+	default:
+		panic(fmt.Sprintf("unknown blend mode %d", mode))
+	}
 }
