7b042123acc192c7e4514a031291932405b7475a
Poletansky Viktor <poletansky@gmail.com>
2024-04-14 12:04:15 +0300

    [master] u

--- blend.go
+++ blend.go
@@ -13,10 +13,10 @@ const (
 
 func Blend(src, dest image.Image, mode BlendMode) image.Image {
 	img := image.NewRGBA(src.Bounds())
-	for i := range Range(src.Bounds().Max.X) {
-		for j := range Range(src.Bounds().Max.Y) {
-			switch mode {
-			case Add:
+	switch mode {
+	case Add:
+		for i := range Range(src.Bounds().Max.X) {
+			for j := range Range(src.Bounds().Max.Y) {
 				if r, g, b, a := src.At(i, j).RGBA(); (color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}) == Black {
 					img.Set(i, j, dest.At(i, j))
 				} else {
