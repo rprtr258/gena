31346db35ce320195bd1eae92800c22c1264b88b
Poletansky Viktor <poletansky@gmail.com>
2024-04-15 10:10:09 +0300

    [update-readme] fix sine

--- cmd/sine.go
+++ cmd/sine.go
@@ -6,18 +6,20 @@ func sine() {
 	const W = 1200
 	const H = 60
 	dc := NewContext(complex(W, H))
-	// dc.SetHexColor("#FFFFFF")
-	// dc.Clear()
-	dc.TransformAdd(Translate(complex(0.95, 0.75)))
-	dc.TransformAdd(Scale(complex(W, H) / 2))
-	for i, a := range RangeF64(0, 2*PI*8, W) {
+	dc.SetColor(White)
+	dc.Clear()
+
+	for i, a := range RangeF64(0, 8*PI*2, W) {
 		dc.LineTo(complex(float64(i), (Sin(a)+1)/2*H))
 	}
 	dc.ClosePath()
+
 	dc.SetColor(ColorHex("#3E606F"))
 	dc.FillPreserve()
+
 	dc.SetColor(ColorHex("#19344180"))
 	dc.SetLineWidth(8)
 	dc.Stroke()
+
 	SavePNG("sine.png", dc.Image())
 }
