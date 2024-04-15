a6242ff7f6c7ac92954127ee5e95cad2db517e53
Poletansky Viktor <poletansky@gmail.com>
2024-04-15 08:10:04 +0300

    [update-readme] fix ellipse

--- cmd/ellipse.go
+++ cmd/ellipse.go
@@ -4,17 +4,16 @@ import . "github.com/rprtr258/gena"
 
 func ellipse() {
 	const S = 1024
-	dc := NewContext(complex(S, S))
+	dc := NewContext(Diag(S))
 	dc.SetColor(ColorRGBA(0, 0, 0, 0.1))
-	for i := 0; i < 360; i += 15 {
+	dc.TransformAdd(Translate(Diag(S) / 2))
+	for _, a := range RangeF64(0, 2*PI, 24) {
 		dc.Stack(func(dc *Context) {
-			dc.WithTransform(Translate(complex(S/2, S/2)), func(dc *Context) {
-				dc.TransformAdd(Rotate(Radians(float64(i))))
-			})
-			dc.DrawEllipse(complex(S/2, S/2), complex(S*7/16, S/8))
+			dc.TransformAdd(Rotate(a))
+			dc.DrawEllipse(0, complex(S*7/16, S/8))
 			dc.Fill()
 		})
 	}
-	dc.DrawImageAnchored(Load("cmd/gopher.png"), complex(S, S)/Coeff(2), complex(0.5, 0.5))
+	dc.DrawImageAnchored(Load("cmd/gopher.png"), 0, complex(0.5, 0.5))
 	SavePNG("ellipse.png", dc.Image())
 }
