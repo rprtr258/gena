4a362fecc1e513f87e9edb852f79165b365d78a1
Poletansky Viktor <poletansky@gmail.com>
2024-04-15 09:44:43 +0300

    [update-readme] fix openfill

--- cmd/openfill.go
+++ cmd/openfill.go
@@ -3,20 +3,28 @@ package main
 import . "github.com/rprtr258/gena"
 
 func openfill() {
-	dc := NewContext(complex(1000, 1000))
-	for _, f := range RangeV2_2(10, 10) {
-		a1 := PI * Random() * 2
-		a2 := PI * (Random() + 0.5)
-		dc.DrawArc(f*100+50, 40, a1, a1+a2)
-		// dc.ClosePath()
+	const N = 10
+	const SZ = 1000
+	dc := NewContext(Diag(SZ))
+	dc.TransformAdd(Translate(Diag(SZ / N / 2)))
+	for _, y := range RangeF64(0, SZ, N) {
+		for _, x := range RangeF64(0, SZ, N) {
+			f := complex(x, y)
+			a1 := PI * Random() * 2
+			a2 := PI * (Random() + 0.5)
+			dc.DrawArc(f, 40, a1, a1+a2)
+		}
 	}
-	dc.SetColor(ColorRGB(0, 0, 0))
+	dc.SetColor(Black)
 	dc.FillPreserve()
-	dc.SetColor(ColorRGB(1, 1, 1))
+
+	dc.SetColor(White)
 	dc.SetLineWidth(8)
 	dc.StrokePreserve()
-	dc.SetColor(ColorRGB(1, 0, 0))
+
+	dc.SetColor(Red)
 	dc.SetLineWidth(4)
 	dc.StrokePreserve()
+
 	SavePNG("openfill.png", dc.Image())
 }
